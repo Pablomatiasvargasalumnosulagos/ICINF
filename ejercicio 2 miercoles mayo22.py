eab00ddcdd6aaea6cb4d16d901a1d75c44ab0b47
Pablomatiasvargasalumnosulagos <pablomatias.vargas@alumnos.ulagos.cl>
2024-05-15 13:01:14 -0400

    Add files via upload

---
+++ ejercicio 2 miercoles mayo22.py
@@ -0,0 +1,12 @@
+nom1=str(input("Ingrese un nombre : "))
+nom2=str(input("Ingrese un nombre : "))
+if nom1 < nom2:
+    print("El orden de los nombres es: ")
+    print( nom1 )
+    print( nom2 )
+
+else:
+    if nom2 < nom1:
+        print("El orden de los nombres es: ")
+        print( nom2 )
+        print( nom1 )
