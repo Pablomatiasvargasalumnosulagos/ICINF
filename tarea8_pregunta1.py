e78394fb2773eb31fd5d8928316e5357155ad4ac
Pablomatiasvargasalumnosulagos <pablomatias.vargas@alumnos.ulagos.cl>
2024-07-10 16:45:06 -0400

    Add files via upload

---
+++ tarea8_pregunta1.py
@@ -0,0 +1,6 @@
+def area_rectangulo(base,altura):
+   res = base * altura
+   return res   
+   
+print(area_rectangulo(15,10))
+
---
+++ tarea8_pregunta2.py
@@ -0,0 +1,6 @@
+def area_circulo(r):
+ pi = 3.14159
+ res = pi * (r**2)
+ return res
+
+print(area_circulo(5)) 
